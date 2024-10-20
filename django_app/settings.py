a9893515984f690ca287eda0c2c72cff594b6cd4
abhishek <abhishekabhishek1208@gmail.com>
2024-10-20 17:17:16 +0530

    changes

--- django_app/settings.py
+++ django_app/settings.py
@@ -29,7 +29,7 @@ DEBUG = os.environ.get('DEBUG',False).lower()== True
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
